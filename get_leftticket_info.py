bdd10ac7684aa01c0b0a8cb30a918619926b0826
Kwinner <chenkai.c.k@hotmail.com>
2018-07-04 01:08:56 +0800

    包含车站地图获取模块，车次车票余票信息获取模块。

---
+++ get_leftticket_info.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# author: Kwinner Chen
+
+import requests, json
+
+url = 'https://kyfw.12306.cn/otn/leftTicket/query'
+
+def get_info_dict(data, from_station, to_station):
+
+    payload = {
+        'leftTicketDTO.train_date':'%s' % data,
+        'leftTicketDTO.from_station':'%s' % from_station,
+        'leftTicketDTO.to_station':'%s' % to_station,
+        'purpose_codes':'ADULT',
+    }
+    header = {
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
+    }
+    resp = requests.get(url, params=payload, headers=header)
+    jsn = resp.text
+    dic = json.loads(jsn)
+    return dic
+
+def page_dict(dic):
+    dic_info = dict()
+    l = dic.get('data').get('result')
+    for s in l:
+        l = s.split('|')     
+        if l[3][0] in ('K', 'Z', 'T'):
+            dic_info[l[3]] = dict(软卧=l[23], 硬卧=l[28], 硬座=l[29], 无座=l[26])
+        if l[3][0] in ('D', 'G'):
+            dic_info[l[3]] = dict(特等商务座=l[32], 一等座=l[31], 二等座=l[30], 无座=l[26]) 
+    return dic_info
+
+def leftTicket_info(data, from_station, to_station):
+    '''
+       获取指定日期，车站间的车票的余票信息
+       参数说明：
+       data: str, 格式为：Y-M-D;
+       from_station: str, 出发车站名类似'BJP', 与官方相同；
+       to_station: 与from_station格式相同。
+    '''
+
+    dic = get_info_dict(data, from_station, to_station)
+    info = page_dict(dic)
+    return info
+    
+
+if __name__ == '__main__':
+    dic = leftTicket_info('2018-07-04', 'BJP', 'SJP')
+    print(dic)
---
+++ station_map/get_map.py
@@ -0,0 +1,84 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# author: Kwinner Chen
+
+import requests, json, os
+
+
+class StationMap(dict, object):
+
+    '''
+       对象本身包含完整python字典功能。当获取一个车站名时，首先试图由对象本身获取，如果过不存在则优先加载本地文件再返回，
+       若本地文件也不存在，则先从网络获取，解析后返回目标值，并存储到本地已被后用。
+       >>> map_dic = StationMap()  # 也可使用类方法get_dict()来获取对象StationMap.get_dict()
+       >>> map_dic['北京'] = 'BJP'
+    '''
+
+    def __init__(self):
+        super().__init__()
+        self.file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'station_map.json')
+
+    @classmethod
+    def get_dict(cls):
+        '''该方法返回一个对象本身的实例'''
+        
+        return cls()
+
+    def __load_dict(self):  # 由本地文件加载到对象本身
+        with open(self.file_path, 'r', encoding='utf-8') as f:
+            dic = json.load(f)
+            self.update(dic)
+            return self
+
+    def __refresh_map(self):  # 从网络获取地图字符串
+        url = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.9056'
+        header = {'User-Agent':'Mozilla/5.0'}
+        try:
+            resp = requests.get(url, headers=header)
+            resp.raise_for_status
+            string = resp.text
+            return string
+        except requests.HTTPError as e:
+            print('跟新车站地图时出错，信息时%s!' % e)
+
+    def __string_paser(self, string):  # 解析字典字符串，并加载到对象本身
+        for i in string.split('@')[1:]:
+            l_i = i.split('|')
+            self[l_i[1]] = l_i[2]
+        return self
+        
+    def __storage_map(self):  # 将对象本身存储到本地，已被后用
+        with open(self.file_path, 'w', encoding='utf-8') as f:
+            json.dump(self, f)
+
+    def __getitem__(self, n):  # 获取目标key，已加载的且存在返回，否则加载本地文件，文件不存在更新
+        r = self.get(n)
+        if r:
+            return r
+        else:
+            if os.path.exists(self.file_path) and os.path.isfile(self.file_path) and os.path.getsize(self.file_path) > 0:
+                self.__load_dict()    
+                return self.get(n)
+            else:
+                string = self.__refresh_map()
+                self.__string_paser(string)
+                self.__storage_map()
+                return self.get(n)
+        # try:
+        #     r = self.get(n)
+        #     if r:
+        #         return r
+        #     else:
+        #         raise KeyError
+        # except KeyError:
+        #     if os.path.exists(self.file_path) and os.path.isfile(self.file_path) and os.path.getsize(self.file_path) > 0:
+        #         self.__load_dict()    
+        #         return self.get(n)
+        #     else:
+        #         string = self.__refresh_map()
+        #         self.__string_paser(string)
+        #         self.__storage_map()
+        #         return self.get(n)
+if __name__ == '__main__':
+    map_dic = StationMap()
+    print(map_dic['北京'])
